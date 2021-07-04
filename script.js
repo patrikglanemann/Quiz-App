5d624f8b7cd86b9ebee06188d06ee31701ffe23e
PatrikG <Patrik0Gn@gmail.com>
2021-07-04 21:26:04 +0200

    Implement button class toggle and button Name

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const listOfBookmarks = document.querySelectorAll(".quiz-card__bookmark");
+const listOfButtons = document.querySelectorAll(".quiz-card__button");
+const listOfCards = document.querySelectorAll(".quiz-card");
 
 for (let i = 0; i < listOfBookmarks.length; i++) {
   const bookmark = listOfBookmarks[i];
@@ -8,5 +10,19 @@ for (let i = 0; i < listOfBookmarks.length; i++) {
     } else {
       bookmark.textContent = "bookmark";
     }
+  });
+}
+
+for (let i = 0; i < listOfButtons.length; i++) {
+  const button = listOfButtons[i];
+  const card = listOfCards[i];
+  button.addEventListener("click", () => {
+    if (button.textContent === "Show Answer") {
+      button.textContent = "Hide Answer";
+    } else {
+      button.textContent = "Show Answer";
+    }
+    button.classList.toggle("quiz-card__button--toggled");
+    card.classList.toggle("quiz-card--opened");
   });
 }
