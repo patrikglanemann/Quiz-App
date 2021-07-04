04ceff0f1597a578e5c9c67255a672ad651174fc
PatrikG <Patrik0Gn@gmail.com>
2021-07-05 00:07:08 +0200

    Output form data to console

--- script.js
+++ script.js
@@ -26,3 +26,34 @@ for (let i = 0; i < listOfButtons.length; i++) {
     card.classList.toggle("quiz-card--opened");
   });
 }
+
+//=============Create Page=====
+
+const data = [];
+const formElement = document.querySelector(".create_form");
+
+formElement.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const question = formElement.add_question.value;
+  const answer = formElement.add_answer.value;
+  const tags = formElement.add_tags.value;
+
+  const newQuestion = {
+    question: question, //Could be typed only "question", since the const-name and the key-name are the same!
+    answer: answer,
+    tags: tags,
+  };
+  if (!data.length) {
+    pushdata(newQuestion);
+  } else {
+    data.length = 0;
+    pushdata(newQuestion);
+  }
+
+  formElement.reset();
+});
+
+function pushdata(object) {
+  data.push(object);
+  console.log(data);
+}
