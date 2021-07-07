3fd592d213974e6d26639233b9f30f60d0ed4a99
PatrikG <Patrik0Gn@gmail.com>
2021-07-07 22:11:13 +0200

    Move boomark click eventListener to cardCreate function

--- index.js
+++ index.js
@@ -11,20 +11,3 @@ const cardSectionElement = document.querySelector(".content");
 cardSections.forEach((cardSetion) => {
   cardSectionElement.append(cardSetion);
 });
-
-//=============Bookmark toggle=====
-
-const listOfBookmarks = document.querySelectorAll(".quiz-card__bookmark");
-const listOfButtons = document.querySelectorAll(".quiz-card__button");
-const listOfCards = document.querySelectorAll(".quiz-card");
-
-for (let i = 0; i < listOfBookmarks.length; i++) {
-  const bookmark = listOfBookmarks[i];
-  bookmark.addEventListener("click", () => {
-    if (bookmark.textContent === "bookmark") {
-      bookmark.textContent = "bookmark_border";
-    } else {
-      bookmark.textContent = "bookmark";
-    }
-  });
-}
