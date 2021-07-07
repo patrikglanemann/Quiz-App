a19cbf65d2216bbc77f418d26ebe3cfcbe1e42f7
PatrikG <Patrik0Gn@gmail.com>
2021-07-07 21:41:08 +0200

    Move button click eventListener to cardCreate function and fix show/hide answers

--- index.js
+++ index.js
@@ -28,17 +28,3 @@ for (let i = 0; i < listOfBookmarks.length; i++) {
     }
   });
 }
-
-for (let i = 0; i < listOfButtons.length; i++) {
-  const button = listOfButtons[i];
-  const card = listOfCards[i];
-  button.addEventListener("click", () => {
-    if (button.textContent === "Show Answer") {
-      button.textContent = "Hide Answer";
-    } else {
-      button.textContent = "Show Answer";
-    }
-    button.classList.toggle("quiz-card__button--toggled");
-    card.classList.toggle("quiz-card--opened");
-  });
-}
