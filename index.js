0aefb93501c6cc7f2be5f8d44ed639f55ba80b75
PatrikG <Patrik0Gn@gmail.com>
2021-07-08 22:30:08 +0200

    Fix typos, store and display new created cards

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import { cardCreate } from "./lib/card-create.js";
-import { data } from "./lib/db.js";
+//import { data } from "./lib/db.js";
+import { getCards } from "./lib/db.js";
 
+//Create Cards
 const cardSections = [];
-data.forEach((cardData) => {
+getCards().forEach((cardData) => {
   const cardSetion = cardCreate(cardData);
   cardSections.push(cardSetion);
 });
 
+//Add Cards
 const cardSectionElement = document.querySelector(".content");
 cardSections.forEach((cardSetion) => {
   cardSectionElement.append(cardSetion);
